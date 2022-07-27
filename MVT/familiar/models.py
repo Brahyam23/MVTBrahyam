52d4358b7f056a2da5b6cd269f5b2fbd8bdfef7b
Brahyam Leiva <brahyamleiva@gmail.com>
2022-07-27 02:26:35 -0300

    Creamos al padre

--- MVT/familiar/models.py
+++ MVT/familiar/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Familiar(models.Model):
 
     nombre = models.CharField(max_length=30)
-    edad = models.IntegerField
-    anio_nac = models.DateField()
+    edad = models.IntegerField()
+    fecha_nac = models.DateField()
--- MVT/familiar/views.py
+++ MVT/familiar/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from familiar.models import Familiar
 
-# Create your views here.
+
+def padre(self):
+
+    padre = Familiar(nombre="Luis", edad="24", fecha_nac="1971-08-12")
+    padre.save()
+
+    ret = f"Soy el padre de la familia, mi nombre es {padre.nombre}, nací el {padre.fecha_nac} y tengo {padre.edad} años."
+
+    return HttpResponse(ret)
